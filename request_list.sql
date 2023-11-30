cb6c4f53a1fc50b8c757bd44951c5db067ac0f87
Levon <ch345@mail.ru>
2023-11-30 17:05:19 +0300

    Update request_list.sql

--- request_list.sql
+++ request_list.sql
@@ -4,8 +4,7 @@ SELECT wp_posts.ID                            AS ID,
        wp_postmeta.meta_value                 AS `Page Template`,
        CONCAT('site_url', wp_posts.post_name) AS `Page Link`
 FROM wp_posts
-         LEFT JOIN
-     wp_postmeta ON (wp_posts.ID = wp_postmeta.post_id)
+  LEFT JOIN wp_postmeta ON (wp_posts.ID = wp_postmeta.post_id)
 WHERE wp_posts.post_type = 'page'
   AND wp_postmeta.meta_key = '_wp_page_template'
   AND wp_posts.post_status = 'publish'
